5a262dd4c5ffcab2f2b33b7bc02227a185a01dfa
Shabbir <shabbirdk05@gmail.com>
2026-04-07 19:32:58 +0530

    "Adding recursion method to call the given number before it exist"

--- src/shabbir/com/Nto1.java
+++ src/shabbir/com/Nto1.java
@@ -2,7 +2,7 @@ package shabbir.com;
 
 public class Nto1 {
     static void main(String[] args) {
-        fun(5);
+        funRev(5);
 
     }
     static void fun(int n){
@@ -11,5 +11,13 @@ public class Nto1 {
         }
         System.out.print(n);
         fun(n-1);
+    }
+    static void funRev(int n){
+        if (n==0){
+            return;
+        }
+
+        funRev(n-1);
+        System.out.print(n);
     }
 }
