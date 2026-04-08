8e8a908646f15246c82552f5cee1cc93f5b9e828
Shabbir <shabbirdk05@gmail.com>
2026-04-08 23:23:36 +0530

    "Finding digit that will be Contains in the arr or not"

---
+++ src/shabbir/com/Firstone.java
@@ -0,0 +1,27 @@
+package shabbir.com;
+
+public class Firstone {
+    static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        int target=5;
+        System.out.println(findIndex(arr,target,0));
+        System.out.println(targets(arr,target,0));
+
+    }
+    static boolean targets(int[] arr,int target,int index ){
+        if (index==arr.length){
+            return false;
+        }
+        return arr[index]==target || targets(arr,target,index+1);
+    }
+    static int findIndex(int[] arr, int target,int index){
+        if (index==arr.length){
+            return -1;
+        }
+        if (arr[index]== target){
+            return index;
+        }else {
+            return findIndex(arr,target,index+1);
+        }
+    }
+}
