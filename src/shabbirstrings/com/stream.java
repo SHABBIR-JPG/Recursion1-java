9f2a5530e284901ac114c2ccfd87b52190697479
Shabbir <shabbirdk05@gmail.com>
2026-04-17 00:24:34 +0530

    "Strings using in recursion to subsets in recursion Expected OUTPUT:[ab,ab,ac,a,bc,b,c,abc]"

--- src/shabbirstrings/com/stream.java
+++ src/shabbirstrings/com/stream.java
@@ -2,18 +2,18 @@ package shabbirstrings.com;
 
 public class stream {
     static void main(String[] args) {
-        System.out.println(skipApple("bacapplegf"));
+        System.out.println(skipAppNotApple("bacapplegf"));
 
     }
-    static String skipApple( String up){
+    static String skipAppNotApple( String up){
         if (up.isEmpty()){
             return "";
         }
 
-        if (up.startsWith("apple")){
-           return skipApple(up.substring(5));
+        if (up.startsWith("app") && !up.startsWith("apple")){
+           return skipAppNotApple(up.substring(3));
         }else {
-            return up.charAt(0)+skipApple(up.substring(1));
+            return up.charAt(0)+skipAppNotApple(up.substring(1));
         }
     }
 }
