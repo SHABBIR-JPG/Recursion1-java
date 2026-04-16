2fc8ae6317b984571f6bb3906e43341c1f0ddf92
Shabbir <shabbirdk05@gmail.com>
2026-04-16 23:52:55 +0530

    "Strings using in recursion to skipping Strings Expected OUTPUT:[bacgf]"

--- src/shabbirstrings/com/stream.java
+++ src/shabbirstrings/com/stream.java
@@ -2,20 +2,18 @@ package shabbirstrings.com;
 
 public class stream {
     static void main(String[] args) {
-        System.out.println(skip("baccad"));
+        System.out.println(skipApple("bacapplegf"));
 
     }
-    static String skip( String up){
+    static String skipApple( String up){
         if (up.isEmpty()){
             return "";
         }
 
-        char ch = up.charAt(0);
-
-        if (ch == 'a'){
-           return skip(up.substring(1));
+        if (up.startsWith("apple")){
+           return skipApple(up.substring(5));
         }else {
-            return ch+skip(up.substring(1));
+            return up.charAt(0)+skipApple(up.substring(1));
         }
     }
 }
