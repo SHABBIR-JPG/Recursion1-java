b8aa49525e5b53ebd49fe2312320e05687fe4fac
Shabbir <shabbirdk05@gmail.com>
2026-04-20 10:20:57 +0530

    "."

---
+++ src/shabbirstrings/com/Ascii.java
@@ -0,0 +1,18 @@
+package shabbirstrings.com;
+
+public class Ascii {
+    static void main(String[] args) {
+       Ascii("","abc");
+    }
+    static void Ascii(String p, String up){
+        if (up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        Ascii(p+ch,up.substring(1));
+        Ascii(p,up.substring(1));
+        Ascii(p + (ch + 0),up.substring(1));
+    }
+}
+    
